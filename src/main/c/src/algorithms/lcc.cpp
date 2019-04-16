813f6a90cc034a82c2ad958d66a0ec4192249c38
Gabor Szarnyas <szarnyasg@gmail.com>
2019-04-16 18:51:16 +0200

    Cleanup code

--- src/main/c/src/algorithms/lcc.cpp
+++ src/main/c/src/algorithms/lcc.cpp
@@ -42,7 +42,7 @@ void WriteOutLCCResult(BenchmarkParameters parameters, IndexMap mapping, GrB_Vec
 /**
  * // Z = f(x) * f(x) - 1
  * This operator calculates the comb(d(v), 2) value.
- * Note: the 2 division can be skipped, because it will be the final division in the LCC calculation
+ * Note: the division by two can be skipped, because it will be the final division in the LCC calculation
  * @param z
  * @param x
  */
@@ -65,15 +65,14 @@ void Lcc(BenchmarkParameters benchmarkParameters) {
     // The size of Adj
     unsigned long n;
 
-    // Get the number of vertices, or size of the adjacency matrix.
+    // Get the number of vertices (the size of the adjacency matrix)
     OK(GrB_Matrix_nrows(&n, A));
     WriteOutDebugMatrix("A", A);
 
     printf("Processing starts at: %lu\n", GetCurrentMilliseconds());
 
     /*
-     * Create unidirected C matrix, by A (+) At
-     * over a boolean binary operator
+     * Create undirected C matrix, by computing A (+) At using a boolean binary operator
      */
     GrB_Matrix At;
     OK(GrB_Matrix_new(&At, GrB_FP64, n, n));
@@ -164,103 +163,6 @@ void Lcc(BenchmarkParameters benchmarkParameters) {
 
     GrB_finalize();
 }
-
-//void LccUndirected(BenchmarkParameters benchmarkParameters) {
-//    GrB_init(GrB_NONBLOCKING);
-//
-//    GrB_Matrix A;
-//    std::cout << "Loading" << std::endl;
-//    IndexMap mapping = ReadMatrix(benchmarkParameters, A);
-//
-//    // Variable required by the OK macro
-//    unsigned int info;
-//
-//    // The size of Adj
-//    unsigned long n;
-//    OK(GrB_Matrix_nrows(&n, A))
-//    WriteOutDebugMatrix("A", A);
-//
-//    std::cout << "Processing starts at: " << GetCurrentMilliseconds() << std::endl;
-//
-//    /*
-//     * Calculate A derivatives
-//     */
-//
-//    // Row sum of A
-//    GrB_Vector Ar;
-//    OK(GrB_Vector_new(&Ar, GrB_FP64, n))
-//    OK(GrB_Matrix_reduce_Monoid(Ar, nullptr, nullptr, GxB_PLUS_FP64_MONOID, A, nullptr))
-//    WriteOutDebugVector("Ar", Ar);
-//
-//    /*
-//     * Calculate wedges
-//     */
-//
-//    // Create an unary operator for calculating combinations
-//
-//    GrB_UnaryOp combinationOp;
-//    GrB_UnaryOp_new(&combinationOp, &CalculateCombinations, GrB_FP64, GrB_FP64);
-//
-//    // Create vector W for containing number of wedges per vertex
-//    GrB_Vector W;
-//    OK(GrB_Vector_new(&W, GrB_FP64, n))
-//    OK(GrB_Vector_apply(W, nullptr, nullptr, combinationOp, Ar, nullptr))
-//    WriteOutDebugVector("W", W);
-//
-//    /*
-//     * Calculate triangles
-//     */
-//
-//    // A^2 matrix
-//    GrB_Matrix A2;
-//    OK(GrB_Matrix_new(&A2, GrB_FP64, n, n))
-//    OK(GrB_mxm(A2, A, nullptr, GxB_PLUS_TIMES_FP64, A, A, nullptr))
-//    WriteOutDebugMatrix("A2", A2);
-//
-//    // ~A^3 matrix with element wise multiplication between A2*A
-//    GrB_Matrix A2A;
-//    OK(GrB_Matrix_new(&A2A, GrB_FP64, n, n))
-//    OK(GrB_eWiseMult_Matrix_Monoid(A2A, nullptr, nullptr, GxB_TIMES_FP64_MONOID, A2, A, nullptr))
-//    WriteOutDebugMatrix("A2A", A2A);
-//
-//    // Determine triangles by A2A row sum
-//    GrB_Vector Tr;
-//    OK(GrB_Vector_new(&Tr, GrB_FP64, n))
-//    OK(GrB_Matrix_reduce_Monoid(Tr, nullptr, nullptr, GxB_PLUS_FP64_MONOID, A2A, nullptr))
-//    WriteOutDebugVector("Tr", Tr);
-//
-//    /*
-//     * Calculate LCC
-//     */
-//    // Create an unary operator for calculating combinations
-//    GrB_Monoid lccDivMonoid;
-//    GrB_Monoid_new_FP64(&lccDivMonoid, GrB_DIV_FP64, 1.0);
-//
-//    GrB_Vector LCC;
-//    OK(GrB_Vector_new(&LCC, GrB_FP64, n))
-//    OK(GrB_eWiseMult_Vector_Monoid(LCC, nullptr, nullptr, lccDivMonoid, Tr, W, nullptr))
-//    WriteOutDebugVector("LCC", LCC);
-//
-//    std::cout << "Processing ends at: " << GetCurrentMilliseconds() << std::endl;
-//
-//    WriteOutLCCResult(benchmarkParameters, mapping, LCC);
-//
-//    GrB_UnaryOp_free(&combinationOp);
-//    GrB_Matrix_free(&A);
-//    GrB_Matrix_free(&A2);
-//    GrB_Matrix_free(&A2A);
-//    GrB_Vector_free(&Ar);
-//    GrB_Vector_free(&W);
-//    GrB_Vector_free(&Tr);
-//    GrB_Vector_free(&LCC);
-//    GrB_Monoid_free(&lccDivMonoid);
-//
-//#ifdef PRINT_RESULT
-//    WriteOutDebugMatrix("A2A", A2A);
-//#endif
-//
-//    GrB_finalize();
-//}
 
 int main(int argc, char **argv) {
     BenchmarkParameters benchmarkParameters = ParseCommandLineParameters(argc, argv);
