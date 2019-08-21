e5a2e8e7e6af560d53f9f80f1723302106207175
Gabor Szarnyas <szarnyasg@gmail.com>
2019-08-21 16:14:16 +0200

    Configure benchmark properties automatically in init script

--- init.sh
+++ init.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+GRAPHS_DIR=${1:-~/graphs}
 PROJECT=graphalytics-1.0.0-graphblas-0.1-SNAPSHOT
 
 rm -rf $PROJECT
@@ -9,4 +10,6 @@ mvn package
 tar xf $PROJECT-bin.tar.gz
 cd $PROJECT/
 cp -r config-template config
+sed -i "s|^graphs.root-directory =$|graphs.root-directory = $GRAPHS_DIR|g" config/benchmark.properties
+sed -i "s|^graphs.validation-directory =$|graphs.validation-directory = $GRAPHS_DIR|g" config/benchmark.properties
 bin/sh/compile-benchmark.sh
