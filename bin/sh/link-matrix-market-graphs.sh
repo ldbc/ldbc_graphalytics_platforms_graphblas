26dae29f37dadd6ccb834a7ab513776c09d69ba4
Gabor Szarnyas <szarnyasg@gmail.com>
2019-10-23 13:08:45 +0200

    Add script to symlink Matrix Market files in the required directories
    Fixes #10

---
+++ bin/sh/link-matrix-market-graphs.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Usage: bin/sh/link-matrix-market-graphs.sh <directory-of-matrix-market-files>'
+    exit 0
+fi
+
+MM_DIR=$1
+
+# make sure files are extracted
+pushd .
+cd $MM_DIR
+7z x -aos '*.7z'
+7z x -aos '*.7z?dl=1'
+popd
+
+mkdir -p intermediate
+
+# unweighted graphs
+for m in $MM_DIR/*-bool.mtx; do
+    # get base filename without path
+    f=$(basename -- "$m")
+    
+    # drop extension and postfix: "-bool.mtx" is 9 characters
+    g="${f::-9}"
+
+    # create directory and add symlinks
+    mkdir -p intermediate/$g
+    ln -s $MM_DIR/$g-bool.mtx intermediate/$g/graph.mtx
+    ln -s $MM_DIR/$g-bool.vtx intermediate/$g/graph.vtx
+    echo $m linked
+done
+
+# weighted graphs
+for m in $MM_DIR/*-fp64.mtx; do
+    # get base filename without path
+    f=$(basename -- "$m")
+    
+    # drop extension and postfix: "-fp64.mtx" is 9 characters
+    g="${f::-9}"
+
+    # create directory and add symlinks
+    mkdir -p intermediate/$g.e_weight
+    ln -s $MM_DIR/$g-bool.mtx intermediate/$g.e_weight/graph.mtx
+    ln -s $MM_DIR/$g-bool.vtx intermediate/$g.e_weight/graph.vtx
+    echo $m linked
+done
