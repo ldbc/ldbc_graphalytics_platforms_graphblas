7f8e1e573e3e74ec0ce54903a29db02aa24bb9e9
Gabor Szarnyas <szarnyasg@gmail.com>
2020-11-19 13:54:12 +0100

    Use MacOS-compatible sed

--- init.sh
+++ init.sh
@@ -15,10 +15,10 @@ cd $PROJECT/
 
 cp -r config-template config
 # set directories
-sed -i "s|^graphs.root-directory =$|graphs.root-directory = $GRAPHS_DIR|g" config/benchmark.properties
-sed -i "s|^graphs.validation-directory =$|graphs.validation-directory = $GRAPHS_DIR|g" config/benchmark.properties
+sed -i.bkp "s|^graphs.root-directory =$|graphs.root-directory = $GRAPHS_DIR|g" config/benchmark.properties
+sed -i.bkp "s|^graphs.validation-directory =$|graphs.validation-directory = $GRAPHS_DIR|g" config/benchmark.properties
 # set the number of threads to use
-sed -i "s|^platform.graphblas.num-threads =$|platform.graphblas.num-threads = $(nproc --all)|g" config/platform.properties
+sed -i.bkp "s|^platform.graphblas.num-threads =$|platform.graphblas.num-threads = $(nproc --all)|g" config/platform.properties
 
 bin/sh/compile-benchmark.sh
 
