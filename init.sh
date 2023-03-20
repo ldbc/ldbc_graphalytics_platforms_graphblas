d6f50ce00e261c1596add8186a84c5197b401e7c
Gabor Szarnyas <szarnyasg@gmail.com>
2023-03-20 10:37:17 +0100

    Revise env vars and CI

--- init.sh
+++ init.sh
@@ -4,27 +4,26 @@ set -eo pipefail
 
 GRAPHS_DIR=${1:-~/graphs}
 MATRICES_DIR=${2:-~/matrices}
-GRAPHALYTICS_VERSION=1.8.0
-PROJECT_VERSION=0.1-SNAPSHOT
-PROJECT=graphalytics-$GRAPHALYTICS_VERSION-graphblas-$PROJECT_VERSION
+
+. project-vars.sh
 
 # cleanup previously compiled wrapper artifacts
 rm -rf bin/exe
 
-rm -rf $PROJECT
+rm -rf ${PROJECT}
 mvn package
-tar xf $PROJECT-bin.tar.gz
-cd $PROJECT/
+tar xf ${PROJECT}-bin.tar.gz
+cd ${PROJECT}
 
 cp -r config-template config
 # set directories
-sed -i.bkp "s|^graphs.root-directory =$|graphs.root-directory = $GRAPHS_DIR|g" config/benchmark.properties
-sed -i.bkp "s|^graphs.validation-directory =$|graphs.validation-directory = $GRAPHS_DIR|g" config/benchmark.properties
+sed -i.bkp "s|^graphs.root-directory =$|graphs.root-directory = ${GRAPHS_DIR}|g" config/benchmark.properties
+sed -i.bkp "s|^graphs.validation-directory =$|graphs.validation-directory = ${GRAPHS_DIR}|g" config/benchmark.properties
 # set the number of threads to use
 sed -i.bkp "s|^platform.graphblas.num-threads =$|platform.graphblas.num-threads = $(nproc --all)|g" config/platform.properties
 
 bin/sh/compile-benchmark.sh
 
-if [ -d $MATRICES_DIR ]; then
-	bin/sh/link-matrix-market-graphs.sh $MATRICES_DIR
+if [ -d ${MATRICES_DIR} ]; then
+	bin/sh/link-matrix-market-graphs.sh ${MATRICES_DIR}
 fi
