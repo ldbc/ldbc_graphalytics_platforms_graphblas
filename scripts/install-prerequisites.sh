162e8c296bee7c6e49862030354e3ecf5546b16e
Gabor Szarnyas <szarnyasg@gmail.com>
2023-05-04 17:52:02 +0200

    CI: Add M1 build

--- scripts/install-prerequisites.sh
+++ scripts/install-prerequisites.sh
@@ -10,6 +10,8 @@ if [[ ! -z $(which yum) ]]; then
 elif [[ ! -z $(which apt-get) ]]; then
     sudo apt-get update
     sudo apt-get install -y python3-pip cmake g++ maven
+elif [ "$(uname)" == "Darwin" ]; then
+    brew install cmake maven coreutils
 fi
 
 pip3 install --user duckdb==0.7.1
